d32adba19ae58fdcd039983db8fd30cf9894976e
Alvin Fong <aktfong@gmail.com>
2020-01-28 22:22:22 -0500

    update RaceResult and its DTO

--- src/com/alvtag/kubkardesktop/models/RaceResult.java
+++ src/com/alvtag/kubkardesktop/models/RaceResult.java
@@ -1,40 +1,22 @@
 package com.alvtag.kubkardesktop.models;
 
 public class RaceResult {
-    private static int heatsCounter = 0;
-
     private final int heatId;
     private final int[] racerIds;
     private int[] raceTimesMs;
 
-    public static void resetRacersCounter() {
-        heatsCounter = 0;
-    }
-
-    public RaceResult(int[] racers) {
-        heatsCounter++;
-        this.heatId = heatsCounter;
-
+    public RaceResult(int[] racers, int[] raceTimesMS, int heatId) {
+        this.heatId = heatId;
         this.racerIds = racers;
         raceTimesMs = new int[racerIds.length];
+
+        this.raceTimesMs = raceTimesMS;
     }
 
     public int[] getRaceTimesMs() {
         return raceTimesMs;
     }
 
-    public void setRacerResults(int[] raceTimesMS) {
-        if (racerIds.length != raceTimesMS.length) {
-            System.out.println("~~~ DUMP ~~~");
-            for (int i = 0; i < raceTimesMS.length; i++) {
-                System.out.println("" + i + ":" + raceTimesMS);
-            }
-            throw new IllegalArgumentException("heat " + heatId + " has " + racerIds.length + " but " +
-                    raceTimesMS.length + " results were reported!");
-        }
-        this.raceTimesMs = raceTimesMS;
-
-    }
 
     /**
      * @return 0 if race has not been finished
--- src/com/alvtag/kubkardesktop/models/RaceResultDTO.java
+++ src/com/alvtag/kubkardesktop/models/RaceResultDTO.java
@@ -1,21 +1,21 @@
 package com.alvtag.kubkardesktop.models;
 
 public class RaceResultDTO {
-    public int laneTime0 = -1;
-    public int laneTime1 = -1;
-    public int laneTime2 = -1;
-    public int laneTime3 = -1;
-    public int laneTime4 = -1;
-    public int laneTime5 = -1;
+    public int l0 = -1;
+    public int l1 = -1;
+    public int l2 = -1;
+    public int l3 = -1;
+    public int l4 = -1;
+    public int l5 = -1;
 
     public int[] toArray() {
         int[] result = new int[6];
-        result[0] = laneTime0;
-        result[1] = laneTime1;
-        result[2] = laneTime2;
-        result[3] = laneTime3;
-        result[4] = laneTime4;
-        result[5] = laneTime5;
+        result[0] = l0;
+        result[1] = l1;
+        result[2] = l2;
+        result[3] = l3;
+        result[4] = l4;
+        result[5] = l5;
         return result;
     }
 }
